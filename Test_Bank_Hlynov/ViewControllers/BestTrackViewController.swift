989ca7ce0a9de53c6ebe3522d55667a8cdeca7ea
seleza1 <koposov.serezha@gmail.com>
2022-11-28 11:17:50 +0300

    create BestVC, BiographyVC

---
+++ Test_Bank_Hlynov/ViewControllers/BestTrackViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BestTrackViewController.swift
+//  Test_Bank_Hlynov
+//
+//  Created by user on 28.11.2022.
+//
+
+import UIKit
+
+class BestTrackViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
