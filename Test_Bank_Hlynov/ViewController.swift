cc24ea78450ccd0f2729a0456d89e279c9641f40
seleza1 <koposov.serezha@gmail.com>
2022-11-28 10:29:34 +0300

    Initial Commit

---
+++ Test_Bank_Hlynov/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test_Bank_Hlynov
+//
+//  Created by user on 28.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
