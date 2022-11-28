2a2ad2254fc5d84b8bfa2b02d074cb7e71c3349c
seleza1 <koposov.serezha@gmail.com>
2022-11-28 22:41:52 +0300

    success getData

--- Test_Bank_Hlynov/NetworkManager/NetworkManager.swift
+++ Test_Bank_Hlynov/NetworkManager/NetworkManager.swift
@@ -9,23 +9,18 @@ import UIKit
 
 
 final class NetworkManager: UIViewController {
-    
-    var welcome: [Tracks] = []
-    
-    let apiKey: String = "f6b4b86d30378ca8d9f43b560d10cdfe"
-    
+
     func getTrack(completion: @escaping(Result<[Tracks], Error>) -> Void) {
         guard let url = URL(string: Url.url) else { return }
         
         URLSession.shared.dataTask(with: url) { data, response, error in
-            guard let error, let response, let data else {
-                completion(.failure(error!))
-                return
-            }
-            print(response)
+            if let error { completion(.failure(error)) }
+            guard let data else { return }
+
+            String(data: data, encoding: .utf8).map { print($0) }
+
             do {
                 let json = try JSONDecoder().decode([Tracks].self, from: data)
-                self.welcome = json
                 DispatchQueue.main.async {
                     completion(.success(json))
                 }
@@ -35,7 +30,6 @@ final class NetworkManager: UIViewController {
             }
         }.resume()
     }
-    
     func fetchImage(from url: URL, completion: @escaping(Result<Data, Error>) -> Void) {
         URLSession.shared.dataTask(with: url) { data, _, error in
             guard let data = data else {
